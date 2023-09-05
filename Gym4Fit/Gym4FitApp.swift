e2b748de4c784ec393172d8906e58c09194cd32a
omerkilicaslan <omerrr_2001@hotmail.com>
2023-09-05 21:57:16 +0300

    Initial Commit

---
+++ Gym4Fit/Gym4FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym4FitApp.swift
+//  Gym4Fit
+//
+//  Created by Ömer Faruk Kılıçaslan on 5.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Gym4FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
